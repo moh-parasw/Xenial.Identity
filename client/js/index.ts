9217b846923f201118b94cb2af24660e4c501e5c
Manuel Grundner <m.grundner@delegate.at>
2020-10-12 09:56:19 +0200

    feat: included xenial template

---
+++ client/js/index.ts
@@ -0,0 +1,4 @@
+import "../css/main.scss";
+import { xenial } from "@xenial-io/xenial-template";
+
+xenial();
---
+++ rollup.config.js
@@ -0,0 +1,103 @@
+import fg from "fast-glob";
+
+import { brotliCompressSync } from "zlib";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import scss from "rollup-plugin-scss";
+import copy from "rollup-plugin-copy";
+import { terser } from "rollup-plugin-terser";
+import gzipPlugin from "rollup-plugin-gzip";
+import filesize from "rollup-plugin-filesize";
+
+const extensions = [".js", ".ts"];
+
+const additionalFiles = () => [
+  `./src/Xenial.Identity/wwwroot/css/bundle.css`,
+  ...fg.sync("./src/Xenial.Identity/wwwroot/css/*.svg"),
+  ...fg.sync("./src/Xenial.Identity/wwwroot/css/*.ttf"),
+  ...fg.sync("./src/Xenial.Identity/wwwroot/img/*.svg"),
+  ...fg.sync("./src/Xenial.Identity/wwwroot/img/**/*.ico"),
+];
+
+export default (commandLineArgs) => {
+  const debug = commandLineArgs.configDebug;
+
+  return [
+    {
+      input: "./client/js/index.ts",
+      output: [
+        {
+          file: `src/Xenial.Identity/wwwroot/js/index.min.js`,
+          format: "iife",
+          plugins: debug ? [] : [terser()],
+        },
+      ],
+      external: [],
+      plugins: [
+        resolve({ extensions }),
+        commonjs(),
+        babel({
+          extensions,
+          exclude: "node_modules/**",
+        }),
+        scss({
+          output: `src/Xenial.Identity/wwwroot/css/bundle.css`,
+          outputStyle: debug ? undefined : "compressed",
+        }),
+        debug
+          ? undefined
+          : copy({
+            targets: [
+              {
+                src: "node_modules/@xenial-io/xenial-template/src/css/*.woff",
+                dest: "./src/Xenial.Identity/wwwroot/css",
+              },
+              {
+                src:
+                  "node_modules/@xenial-io/xenial-template/src/css/*.woff2",
+                dest: "./src/Xenial.Identity/wwwroot/css",
+              },
+              {
+                src: "node_modules/@xenial-io/xenial-template/src/css/*.ttf",
+                dest: "./src/Xenial.Identity/wwwroot/css",
+              },
+              {
+                src: "node_modules/@xenial-io/xenial-template/src/css/*.svg",
+                dest: "./src/Xenial.Identity/wwwroot/css",
+              },
+              {
+                src: "node_modules/@xenial-io/xenial-template/src/img/**/*",
+                dest: "./src/Xenial.Identity/wwwroot/img",
+              },
+            ],
+          }),
+        debug
+          ? undefined
+          : copy({
+            targets: [
+              {
+                src: "client/img",
+                dest: "./src/Xenial.Identity/wwwroot",
+              }
+            ],
+            flatten: false,
+          }),
+        debug
+          ? undefined
+          : gzipPlugin({
+            additionalFiles: additionalFiles(),
+          }),
+        debug
+          ? undefined
+          : gzipPlugin({
+            additionalFiles: additionalFiles(),
+            customCompression: (content) =>
+              brotliCompressSync(Buffer.from(content)),
+            fileName: ".br",
+          }),
+        debug ? undefined : filesize(),
+      ],
+    },
+  ];
+};
