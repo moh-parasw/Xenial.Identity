04504f5c3c55046a2ccb1efdd94ae54aa7690b2e
Manuel Grundner <m.grundner@delegate.at>
2020-10-14 10:09:15 +0200

    fix: easter egg

--- client/js/index.ts
+++ client/js/index.ts
@@ -2,3 +2,9 @@ import "../css/main.scss";
 import { xenial } from "@xenial-io/xenial-template";
 
 xenial();
+
+document.querySelectorAll(".egg").forEach((el: HTMLElement) => {
+    el.onclick = async () => {
+        window.open("https://unsplash.com/s/photos/austria", "_blank");
+    };
+});
