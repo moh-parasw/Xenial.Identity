c8591f0195ef1fd8ca26cd58f030424e21a2ec06
Manuel Grundner <m.grundner@delegate.at>
2020-10-16 16:18:05 +0200

    test: more resource store tests

--- src/Xenial.Identity.Xpo.Storage.Tests/IntegrationTests/ResourceStoreTests.cs
+++ src/Xenial.Identity.Xpo.Storage.Tests/IntegrationTests/ResourceStoreTests.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using DevExpress.Xpo;
-using IdentityModel;
-using IdentityServer4.Models;
-using Xenial.Identity.Xpo.Storage.Stores;
-using Xenial.Identity.Xpo.Storage.Mappers;
-using static Xenial.Tasty;
-using FluentAssertions;
 using System.Linq;
+
+using DevExpress.Xpo;
+
+using FluentAssertions;
+
+using IdentityModel;
+
+using IdentityServer4.Models;
+
+using Xenial.Identity.Xpo.Storage.Mappers;
+using Xenial.Identity.Xpo.Storage.Stores;
+
+using static Xenial.Tasty;
 
 namespace Xenial.Identity.Xpo.Storage.Tests.IntegrationTests
 {
@@ -27,19 +32,19 @@ namespace Xenial.Identity.Xpo.Storage.Tests.IntegrationTests
                 return (new ResourceStore(uow, new FakeLogger<ResourceStore>()), uow);
             }
 
-            // static IdentityResource CreateIdentityTestResource()
-            //     => new IdentityResource()
-            //     {
-            //         Name = Guid.NewGuid().ToString(),
-            //         DisplayName = Guid.NewGuid().ToString(),
-            //         Description = Guid.NewGuid().ToString(),
-            //         ShowInDiscoveryDocument = true,
-            //         UserClaims =
-            //         {
-            //             JwtClaimTypes.Subject,
-            //             JwtClaimTypes.Name,
-            //         }
-            //     };
+            static IdentityResource CreateIdentityTestResource()
+                => new IdentityResource()
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    DisplayName = Guid.NewGuid().ToString(),
+                    Description = Guid.NewGuid().ToString(),
+                    ShowInDiscoveryDocument = true,
+                    UserClaims =
+                    {
+                        JwtClaimTypes.Subject,
+                        JwtClaimTypes.Name,
+                    }
+                };
 
             static ApiResource CreateApiResourceTestResource()
                 => new ApiResource
@@ -54,16 +59,16 @@ namespace Xenial.Identity.Xpo.Storage.Tests.IntegrationTests
                     }
                 };
 
-            // static ApiScope CreateApiScopeTestResource()
-            //     => new ApiScope
-            //     {
-            //         Name = Guid.NewGuid().ToString(),
-            //         UserClaims =
-            //         {
-            //                 Guid.NewGuid().ToString(),
-            //                 Guid.NewGuid().ToString(),
-            //         }
-            //     };
+            static ApiScope CreateApiScopeTestResource()
+                => new ApiScope
+                {
+                    Name = Guid.NewGuid().ToString(),
+                    UserClaims =
+                    {
+                            Guid.NewGuid().ToString(),
+                            Guid.NewGuid().ToString(),
+                    }
+                };
 
             It("FindApiResourcesByNameAsync when Resource exists should return Resource and Collections", async () =>
             {
@@ -89,6 +94,95 @@ namespace Xenial.Identity.Xpo.Storage.Tests.IntegrationTests
                     foundResource.ApiSecrets.Should().NotBeEmpty();
                     foundResource.Scopes.Should().NotBeNull();
                     foundResource.Scopes.Should().NotBeEmpty();
+                }
+            });
+
+            It("FindApiResourcesByNameAsync when Resources exist should only return requested Resources", async () =>
+            {
+                var resource = CreateApiResourceTestResource();
+
+                using (var uow1 = new UnitOfWork(dataLayer))
+                {
+                    await uow1.SaveAsync(resource.ToEntity(uow1));
+                    await uow1.SaveAsync(CreateApiResourceTestResource().ToEntity(uow1));
+                    await uow1.CommitChangesAsync();
+                }
+
+                var (store, uow) = CreateStore();
+                using (uow)
+                {
+                    var foundResource = (await store.FindApiResourcesByNameAsync(new[] { resource.Name })).SingleOrDefault();
+
+                    foundResource.Should().NotBeNull();
+                    foundResource.Name.Should().Be(resource.Name);
+
+                    foundResource.UserClaims.Should().NotBeNull();
+                    foundResource.UserClaims.Should().NotBeEmpty();
+                    foundResource.ApiSecrets.Should().NotBeNull();
+                    foundResource.ApiSecrets.Should().NotBeEmpty();
+                    foundResource.Scopes.Should().NotBeNull();
+                    foundResource.Scopes.Should().NotBeEmpty();
+                }
+            });
+
+            It("FindApiResourcesByScopeNameAsync when Resources exist should return Resources", async () =>
+            {
+                var testApiResource = CreateApiResourceTestResource();
+                var testApiScope = CreateApiScopeTestResource();
+                testApiResource.Scopes.Add(testApiScope.Name);
+
+                using (var uow1 = new UnitOfWork(dataLayer))
+                {
+                    await uow1.SaveAsync(testApiResource.ToEntity(uow1));
+                    await uow1.SaveAsync(testApiScope.ToEntity(uow1));
+                    await uow1.CommitChangesAsync();
+                }
+
+                var (store, uow) = CreateStore();
+                using (uow)
+                {
+                    var resources = await store.FindApiResourcesByScopeNameAsync(new List<string>
+                    {
+                        testApiScope.Name
+                    });
+
+                    resources.Should().NotBeNull();
+                    resources.Should().NotBeEmpty();
+                    resources.SingleOrDefault(x => x.Name == testApiResource.Name)
+                        .Should().NotBeNull();
+                }
+            });
+
+            It("FindApiResourcesByScopeNameAsync when Resources exist should only return Resources requested", async () =>
+            {
+                var testIdentityResource = CreateIdentityTestResource();
+                var testApiResource = CreateApiResourceTestResource();
+                var testApiScope = CreateApiScopeTestResource();
+                testApiResource.Scopes.Add(testApiScope.Name);
+
+                using (var uow1 = new UnitOfWork(dataLayer))
+                {
+                    await uow1.SaveAsync(testIdentityResource.ToEntity(uow1));
+                    await uow1.SaveAsync(testApiResource.ToEntity(uow1));
+                    await uow1.SaveAsync(testApiScope.ToEntity(uow1));
+                    await uow1.SaveAsync(CreateIdentityTestResource().ToEntity(uow1));
+                    await uow1.SaveAsync(CreateApiResourceTestResource().ToEntity(uow1));
+                    await uow1.SaveAsync(CreateApiScopeTestResource().ToEntity(uow1));
+                    await uow1.CommitChangesAsync();
+                }
+
+                var (store, uow) = CreateStore();
+                using (uow)
+                {
+                    var resources = await store.FindApiResourcesByScopeNameAsync(new[]
+                    {
+                        testApiScope.Name
+                    });
+
+                    resources.Should().NotBeNull();
+                    resources.Should().NotBeEmpty();
+                    resources.SingleOrDefault(x => x.Name == testApiResource.Name)
+                        .Should().NotBeNull();
                 }
             });
         });
