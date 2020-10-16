7ad1feb9f660c658f06f89472cfd9243f5407153
Manuel Grundner <m.grundner@delegate.at>
2020-10-16 15:47:37 +0200

    feat: added ResourceStore

---
+++ src/Xenial.Identity.Xpo.Storage/Stores/ResourceStore.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DevExpress.Xpo;
+
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+
+using Microsoft.Extensions.Logging;
+
+using Xenial.Identity.Xpo.Storage.Mappers;
+using Xenial.Identity.Xpo.Storage.Models;
+
+namespace Xenial.Identity.Xpo.Storage.Stores
+{
+    /// <summary>
+    /// Implementation of IResourceStore thats uses EF.
+    /// </summary>
+    /// <seealso cref="IdentityServer4.Stores.IResourceStore" />
+    public class ResourceStore : IResourceStore
+    {
+        /// <summary>
+        /// The DbContext.
+        /// </summary>
+        protected readonly UnitOfWork UnitOfWork;
+
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        protected readonly ILogger<ResourceStore> Logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceStore"/> class.
+        /// </summary>
+        /// <param name="unitOfWork">The unitOfWork.</param>
+        /// <param name="logger">The logger.</param>
+        /// <exception cref="ArgumentNullException">context</exception>
+        public ResourceStore(UnitOfWork unitOfWork, ILogger<ResourceStore> logger)
+        {
+            UnitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            Logger = logger;
+        }
+
+        /// <summary>
+        /// Finds the API resources by name.
+        /// </summary>
+        /// <param name="apiResourceNames">The names.</param>
+        /// <returns></returns>
+        public virtual async Task<IEnumerable<ApiResource>> FindApiResourcesByNameAsync(IEnumerable<string> apiResourceNames)
+        {
+            if (apiResourceNames == null)
+            {
+                throw new ArgumentNullException(nameof(apiResourceNames));
+            }
+
+            var apis = UnitOfWork.Query<XpoApiResource>()
+                .Where(apiResource => apiResourceNames.Contains(apiResource.Name));
+
+            var result = (await apis.ToArrayAsync())
+                //TODO: Not sure if needed
+                //.Where(x => apiResourceNames.Contains(x.Name))
+                .Select(x => x.ToModel()).ToArray();
+
+            if (result.Any())
+            {
+                Logger.LogDebug("Found {apis} API resource in database", result.Select(x => x.Name));
+            }
+            else
+            {
+                Logger.LogDebug("Did not find {apis} API resource in database", apiResourceNames);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets API resources by scope name.
+        /// </summary>
+        /// <param name="scopeNames"></param>
+        /// <returns></returns>
+        public virtual async Task<IEnumerable<ApiResource>> FindApiResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            var names = scopeNames.ToArray();
+
+            var apis =
+                from api in UnitOfWork.Query<XpoApiResource>()
+                where api.Scopes.Where(x => names.Contains(x.Scope)).Any()
+                select api;
+
+            var results = (await apis.ToArrayAsync())
+                  //TODO: Not sure if needed
+                  //.Where(api => api.Scopes.Any(x => names.Contains(x.Scope)))
+                  ;
+            var models = results.Select(x => x.ToModel()).ToArray();
+
+            Logger.LogDebug("Found {apis} API resources in database", models.Select(x => x.Name));
+
+            return models;
+        }
+
+        /// <summary>
+        /// Gets identity resources by scope name.
+        /// </summary>
+        /// <param name="scopeNames"></param>
+        /// <returns></returns>
+        public virtual async Task<IEnumerable<IdentityResource>> FindIdentityResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            var scopes = scopeNames.ToArray();
+
+            var resources =
+                from identityResource in UnitOfWork.Query<XpoIdentityResource>()
+                where scopes.Contains(identityResource.Name)
+                select identityResource;
+
+            var results = (await resources.ToArrayAsync())
+                //TODO: Not sure if needed
+                .Where(x => scopes.Contains(x.Name))
+                ;
+
+            Logger.LogDebug("Found {scopes} identity scopes in database", results.Select(x => x.Name));
+
+            return results.Select(x => x.ToModel()).ToArray();
+        }
+
+        /// <summary>
+        /// Gets scopes by scope name.
+        /// </summary>
+        /// <param name="scopeNames"></param>
+        /// <returns></returns>
+        public virtual async Task<IEnumerable<ApiScope>> FindApiScopesByNameAsync(IEnumerable<string> scopeNames)
+        {
+            var scopes = scopeNames.ToArray();
+
+            var resources =
+                from scope in UnitOfWork.Query<XpoApiScope>()
+                where scopes.Contains(scope.Name)
+                select scope;
+
+            var results = (await resources.ToArrayAsync())
+                //TODO: Not sure if needed
+                //.Where(x => scopes.Contains(x.Name))
+                ;
+
+            Logger.LogDebug("Found {scopes} scopes in database", results.Select(x => x.Name));
+
+            return results.Select(x => x.ToModel()).ToArray();
+        }
+
+        /// <summary>
+        /// Gets all resources.
+        /// </summary>
+        /// <returns></returns>
+        public virtual async Task<Resources> GetAllResourcesAsync()
+        {
+            var identity = UnitOfWork.Query<XpoIdentityResource>();
+
+            var apis = UnitOfWork.Query<XpoApiResource>();
+
+            var scopes = UnitOfWork.Query<XpoApiScope>();
+
+            var result = new Resources(
+                (await identity.ToArrayAsync()).Select(x => x.ToModel()),
+                (await apis.ToArrayAsync()).Select(x => x.ToModel()),
+                (await scopes.ToArrayAsync()).Select(x => x.ToModel())
+            );
+
+            Logger.LogDebug("Found {scopes} as all scopes, and {apis} as API resources",
+                result.IdentityResources.Select(x => x.Name).Union(result.ApiScopes.Select(x => x.Name)),
+                result.ApiResources.Select(x => x.Name));
+
+            return result;
+        }
+    }
+}
