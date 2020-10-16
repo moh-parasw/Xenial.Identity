4e863166022063565e1ef38d9b1c2c139b2fe552
Manuel Grundner <m.grundner@delegate.at>
2020-10-16 17:51:55 +0200

    test: token cleanup tests finished

--- src/Xenial.Identity.Xpo.Storage.Tests/TokenCleanup/TokenCleanupTests.cs
+++ src/Xenial.Identity.Xpo.Storage.Tests/TokenCleanup/TokenCleanupTests.cs
@@ -80,6 +80,95 @@ namespace Xenial.Identity.Xpo.Storage.Tests.TokenCleanup
                     ).Should().BeNull();
                 }
             });
+
+            It("RemoveExpiredGrantsAsync when valid Grants exist expect valid Grants in Db", async () =>
+            {
+                var key = Guid.NewGuid().ToString();
+
+                using (var uow1 = new UnitOfWork(dataLayer))
+                {
+                    var validGrant = new XpoPersistedGrant(uow1)
+                    {
+                        Key = key,
+                        ClientId = "app1",
+                        Type = "reference",
+                        SubjectId = "123",
+                        Expiration = DateTime.UtcNow.AddDays(3),
+                        Data = "{!}"
+                    };
+
+                    await uow1.SaveAsync(validGrant);
+                    await uow1.CommitChangesAsync();
+                }
+
+                await CreateSut().RemoveExpiredGrantsAsync();
+
+                using (var uow2 = new UnitOfWork(dataLayer))
+                {
+                    (await uow2.Query<XpoPersistedGrant>()
+                        .FirstOrDefaultAsync(x => x.Key == key)
+                    ).Should().NotBeNull();
+                }
+            });
+
+            It("RemoveExpiredGrantsAsync when expired DeviceGrants exist expect expired DeviceGrants to be removed", async () =>
+            {
+                var deviceCode = Guid.NewGuid().ToString();
+
+                using (var uow1 = new UnitOfWork(dataLayer))
+                {
+                    var expiredGrant = new XpoDeviceFlowCodes(uow1)
+                    {
+                        DeviceCode = deviceCode,
+                        UserCode = Guid.NewGuid().ToString(),
+                        ClientId = "app1",
+                        SubjectId = "123",
+                        CreationTime = DateTime.UtcNow.AddDays(-4),
+                        Expiration = DateTime.UtcNow.AddDays(-3),
+                        Data = "{!}"
+                    };
+                    await uow1.SaveAsync(expiredGrant);
+                    await uow1.CommitChangesAsync();
+                }
+
+                await CreateSut().RemoveExpiredGrantsAsync();
+
+                using (var uow2 = new UnitOfWork(dataLayer))
+                {
+                    (await uow2.Query<XpoDeviceFlowCodes>()
+                        .FirstOrDefaultAsync(x => x.DeviceCode == deviceCode)
+                    ).Should().BeNull();
+                }
+            });
+
+            It("RemoveExpiredGrantsAsync when valid DeviceGrants exist expect valid DeviceGrants to be in Db", async () =>
+            {
+                var deviceCode = Guid.NewGuid().ToString();
+                using (var uow1 = new UnitOfWork(dataLayer))
+                {
+                    var validGrant = new XpoDeviceFlowCodes(uow1)
+                    {
+                        DeviceCode = deviceCode,
+                        UserCode = "2468",
+                        ClientId = "app1",
+                        SubjectId = "123",
+                        CreationTime = DateTime.UtcNow.AddDays(-4),
+                        Expiration = DateTime.UtcNow.AddDays(3),
+                        Data = "{!}"
+                    };
+                    await uow1.SaveAsync(validGrant);
+                    await uow1.CommitChangesAsync();
+                }
+
+                await CreateSut().RemoveExpiredGrantsAsync();
+
+                using (var uow2 = new UnitOfWork(dataLayer))
+                {
+                    (await uow2.Query<XpoDeviceFlowCodes>()
+                        .FirstOrDefaultAsync(x => x.DeviceCode == deviceCode)
+                    ).Should().NotBeNull();
+                }
+            });
         });
     }
 }
