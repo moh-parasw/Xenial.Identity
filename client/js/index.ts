7eb8b2f054fb1ff27ffa979e88086cf004bd8354
Manuel Grundner <m.grundner@delegate.at>
2020-10-23 09:02:01 +0200

    feat: notifications

--- client/js/index.ts
+++ client/js/index.ts
@@ -1,7 +1,8 @@
 ﻿import "../css/main.scss";
+import 'notyf/notyf.min.css';
 import { xenial } from "@xenial-io/xenial-template";
 import QRCode from "qrcode/build/qrcode";
-
+import { Notyf } from "notyf";
 xenial();
 
 document.querySelectorAll(".egg").forEach((el: HTMLElement) => {
@@ -21,3 +22,19 @@ document.querySelectorAll("[data-qrcode]").forEach((el: HTMLElement) => {
     });
 });
 
+const notyf = new Notyf({
+    position: {
+        x: "center",
+        y: "top"
+    },
+    duration: 2500,
+    dismissible: true
+});
+
+document.querySelectorAll("[data-success]").forEach((el: HTMLElement) => {
+    notyf.success(el.getAttribute("data-success"));
+});
+
+document.querySelectorAll("[data-error]").forEach((el: HTMLElement) => {
+    notyf.error(el.getAttribute("data-error"));
+});
