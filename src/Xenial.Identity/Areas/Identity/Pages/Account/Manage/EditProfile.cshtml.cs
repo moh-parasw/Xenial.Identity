0244683c58a1043d6e5acc523cd218d209f2bcc4
Manuel Grundner <m.grundner@delegate.at>
2020-10-23 12:27:49 +0200

    fix: aside

---
+++ src/Xenial.Identity/Areas/Identity/Pages/Account/Manage/EditProfile.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Xenial.Identity.Areas.Identity.Pages.Account.Manage
+{
+    public class EditProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
