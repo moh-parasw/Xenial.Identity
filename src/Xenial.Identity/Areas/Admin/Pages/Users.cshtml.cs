ef8b7a2c86734e50679e6c2a0f3562e485f2d494
Manuel Grundner <m.grundner@delegate.at>
2020-10-27 10:46:05 +0100

    feat: grid style and user list

--- src/Xenial.Identity/Areas/Admin/Pages/Users.cshtml.cs
+++ src/Xenial.Identity/Areas/Admin/Pages/Users.cshtml.cs
@@ -1,16 +1,44 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+
+using AutoMapper.QueryableExtensions;
+
+using DevExpress.Xpo;
+
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Xenial.Identity.Data;
 
 namespace Xenial.Identity.Areas.Admin.Pages
 {
     public class UsersModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<XenialIdentityUser> userManager;
+
+        public UsersModel(UserManager<XenialIdentityUser> userManager)
+            => this.userManager = userManager;
+
+        public IQueryable<UserOutputModel> Users { get; private set; } = new UserOutputModel[0].AsQueryable();
+        public class UserOutputModel
         {
+            public string Id { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
         }
+
+        public async Task OnGet(CancellationToken cancellationToken = default)
+            => Users = (await userManager.Users.ToListAsync(cancellationToken)).Select(r => new UserOutputModel
+            {
+                Id = r.Id,
+                UserName = r.UserName,
+                FirstName = r.FirstName,
+                LastName = r.LastName,
+            }).AsQueryable();
     }
 }
