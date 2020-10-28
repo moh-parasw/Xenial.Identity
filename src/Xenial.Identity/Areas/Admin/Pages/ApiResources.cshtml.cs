fa57a20369182959bdf3cd87c07cbd0c348a1cd5
Manuel Grundner <m.grundner@delegate.at>
2020-10-28 12:55:48 +0100

    feat: view api resources

--- src/Xenial.Identity/Areas/Admin/Pages/ApiResources.cshtml.cs
+++ src/Xenial.Identity/Areas/Admin/Pages/ApiResources.cshtml.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using DevExpress.Xpo;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Xenial.Identity.Xpo.Storage.Models;
 
 namespace Xenial.Identity.Areas.Admin.Pages
 {
     public class ApiResourcesModel : PageModel
     {
+        private readonly UnitOfWork unitOfWork;
+        public ApiResourcesModel(UnitOfWork unitOfWork)
+            => this.unitOfWork = unitOfWork;
+
+
         public IQueryable<ApiResourceOutputModel> ApiResources { get; private set; } = new ApiResourceOutputModel[0].AsQueryable();
 
         public class ApiResourceOutputModel
         {
-            public string Id { get; set; }
+            public int Id { get; set; }
             public string Name { get; set; }
         }
 
         public void OnGet()
-        {
-        }
+            => ApiResources = unitOfWork.Query<XpoApiResource>().Select(r => new ApiResourceOutputModel
+            {
+                Id = r.Id,
+                Name = r.Name
+            }).AsQueryable();
     }
 }
