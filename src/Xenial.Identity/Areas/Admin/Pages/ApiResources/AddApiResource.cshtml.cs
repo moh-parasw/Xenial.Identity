670ea3a6f0f230c24e54eaf593f6aa3183a1cc4d
Manuel Grundner <m.grundner@delegate.at>
2020-10-29 07:57:30 +0100

    feat: more fields on api resource

--- src/Xenial.Identity/Areas/Admin/Pages/ApiResources/AddApiResource.cshtml.cs
+++ src/Xenial.Identity/Areas/Admin/Pages/ApiResources/AddApiResource.cshtml.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
+using AutoMapper;
+
 using DevExpress.Xpo;
 using DevExpress.Xpo.DB.Exceptions;
 
@@ -28,11 +30,25 @@ namespace Xenial.Identity.Areas.Admin.Pages.ApiResources
         {
             [Required]
             public string Name { get; set; }
+            public string DisplayName { get; set; }
+            public string Description { get; set; }
+            public bool Enabled { get; set; } = true;
+            public bool ShowInDiscoveryDocument { get; set; } = true;
+            public bool NonEditable { get; set; }
         }
 
+        internal class ApiResourceMappingConfiguration : Profile
+        {
+            public ApiResourceMappingConfiguration()
+                => CreateMap<ApiResourceInputModel, XpoApiResource>();
+        }
+
+        internal static IMapper Mapper { get; }
+            = new MapperConfiguration(cfg => cfg.AddProfile<ApiResourceMappingConfiguration>())
+                .CreateMapper();
 
         [Required, BindProperty]
-        public ApiResourceInputModel Input { get; set; }
+        public ApiResourceInputModel Input { get; set; } = new ApiResourceInputModel();
 
         public string StatusMessage { get; set; }
 
@@ -40,12 +56,9 @@ namespace Xenial.Identity.Areas.Admin.Pages.ApiResources
         {
             if (ModelState.IsValid)
             {
-                var apiResource = new XpoApiResource(unitOfWork)
-                {
-                    Name = Input.Name
-                };
                 try
                 {
+                    var apiResource = Mapper.Map(Input, new XpoApiResource(unitOfWork));
                     await unitOfWork.SaveAsync(apiResource);
                     await unitOfWork.CommitChangesAsync();
                     return Redirect("/Admin/ApiResources");
