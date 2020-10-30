e4f54a017b2fcaf07df46dec25a18f0c8cd5503e
Manuel Grundner <m.grundner@delegate.at>
2020-10-30 16:09:07 +0100

    feat: add client stub

---
+++ src/Xenial.Identity/Areas/Admin/Pages/Clients/AddClient.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using DevExpress.Xpo;
+using DevExpress.Xpo.DB.Exceptions;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using Xenial.Identity.Xpo.Storage.Models;
+
+namespace Xenial.Identity.Areas.Admin.Pages.Clients
+{
+    public class AddClientModel : PageModel
+    {
+        private readonly UnitOfWork unitOfWork;
+        private readonly ILogger<AddClientModel> logger;
+        public AddClientModel(UnitOfWork unitOfWork, ILogger<AddClientModel> logger)
+            => (this.unitOfWork, this.logger) = (unitOfWork, logger);
+
+        public class ClientInputModel
+        {
+            [Required]
+            public string Name { get; set; }
+            public string DisplayName { get; set; }
+            public string Description { get; set; }
+            public bool Enabled { get; set; } = true;
+            public bool ShowInDiscoveryDocument { get; set; } = true;
+            public bool NonEditable { get; set; }
+            public string UserClaims { get; set; }
+        }
+
+        internal class ClientMappingConfiguration : Profile
+        {
+            public ClientMappingConfiguration()
+                => CreateMap<ClientInputModel, XpoClient>()
+                ;
+        }
+
+        internal static IMapper Mapper { get; }
+            = new MapperConfiguration(cfg => cfg.AddProfile<ClientMappingConfiguration>())
+                .CreateMapper();
+
+        [Required, BindProperty]
+        public ClientInputModel Input { get; set; } = new ClientInputModel();
+
+        public string StatusMessage { get; set; }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var client = Mapper.Map(Input, new XpoClient(unitOfWork));
+
+                    await unitOfWork.SaveAsync(client);
+                    await unitOfWork.CommitChangesAsync();
+                    return Redirect("/Admin/Clients");
+                }
+                catch (ConstraintViolationException ex)
+                {
+                    logger.LogWarning(ex, "Error saving Client with {Name}", Input?.Name);
+                    ModelState.AddModelError($"{nameof(Input)}.{nameof(Input.Name)}", "Client name must be unique");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error saving Client with {Name}", Input?.Name);
+                    StatusMessage = $"Error saving client: {ex.Message}";
+                    return Page();
+                }
+            }
+
+            StatusMessage = "Error: Check Validation";
+
+            return Page();
+        }
+    }
+}
