63ee96c2a6b1b2bfb79a6c7912fdc131d0ace152
Manuel Grundner <m.grundner@delegate.at>
2020-10-30 11:15:55 +0100

    feat: clients list

--- src/Xenial.Identity/Areas/Admin/Pages/Clients.cshtml.cs
+++ src/Xenial.Identity/Areas/Admin/Pages/Clients.cshtml.cs
@@ -1,16 +1,49 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+using DevExpress.Xpo;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Xenial.Identity.Xpo.Storage.Models;
 
 namespace Xenial.Identity.Areas.Admin.Pages
 {
     public class ClientsModel : PageModel
     {
-        public void OnGet()
+        private readonly UnitOfWork unitOfWork;
+        public ClientsModel(UnitOfWork unitOfWork)
+            => this.unitOfWork = unitOfWork;
+
+        public IQueryable<ClientsOutputModel> Clients { get; set; } = new ClientsOutputModel[0].AsQueryable();
+
+        public class ClientsOutputModel
         {
+            public string Id { get; set; }
+            public string ClientId { get; set; }
+            public string ClientName { get; set; }
         }
+
+        internal class ClientsMappingConfiguration : Profile
+        {
+            public ClientsMappingConfiguration()
+                => CreateMap<ClientsOutputModel, XpoClient>()
+                    .ReverseMap()
+                ;
+        }
+
+        internal static IMapper Mapper { get; }
+            = new MapperConfiguration(cfg => cfg.AddProfile<ClientsMappingConfiguration>())
+                .CreateMapper();
+
+        public void OnGet()
+            => Clients = unitOfWork.Query<XpoClient>().ProjectTo<ClientsOutputModel>(Mapper.ConfigurationProvider);
+
     }
 }
