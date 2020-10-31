6b062ccf9422449be700a9856876ff2ec0e4f7f8
Manuel Grundner <m.grundner@delegate.at>
2020-10-31 09:12:33 +0100

    feat: add client

--- src/Xenial.Identity/Areas/Admin/Pages/Clients/AddClient.cshtml.cs
+++ src/Xenial.Identity/Areas/Admin/Pages/Clients/AddClient.cshtml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,8 +16,44 @@ using Microsoft.Extensions.Logging;
 
 using Xenial.Identity.Xpo.Storage.Models;
 
+using IdentityServer4.Models;
+
 namespace Xenial.Identity.Areas.Admin.Pages.Clients
 {
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public sealed class IconAttribute : Attribute
+    {
+        public string Icon { get; }
+        public IconAttribute(string icon)
+            => Icon = icon;
+    }
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
+    public sealed class HeaderAttribute : Attribute
+    {
+        public string Header { get; }
+        public HeaderAttribute(string header)
+            => Header = header;
+    }
+
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// Gets an attribute on an enum field value
+        /// </summary>
+        /// <typeparam name="T">The type of the attribute you want to retrieve</typeparam>
+        /// <param name="enumVal">The enum value</param>
+        /// <returns>The attribute of type T that exists on the enum value</returns>
+        /// <example><![CDATA[string desc = myEnumVariable.GetAttributeOfType<DescriptionAttribute>().Description;]]></example>
+        public static T GetAttributeOfType<T>(this Enum enumVal) where T : System.Attribute
+        {
+            var type = enumVal.GetType();
+            var memInfo = type.GetMember(enumVal.ToString());
+            var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
+            return (attributes.Length > 0) ? (T)attributes[0] : null;
+        }
+    }
+
     public class AddClientModel : PageModel
     {
         private readonly UnitOfWork unitOfWork;
@@ -26,13 +64,52 @@ namespace Xenial.Identity.Areas.Admin.Pages.Clients
         public class ClientInputModel
         {
             [Required]
-            public string Name { get; set; }
+            public string ClientId { get; set; }
+            [Required]
+            public string ClientName { get; set; }
             public string DisplayName { get; set; }
             public string Description { get; set; }
-            public bool Enabled { get; set; } = true;
-            public bool ShowInDiscoveryDocument { get; set; } = true;
-            public bool NonEditable { get; set; }
-            public string UserClaims { get; set; }
+
+            public ClientTypes ClientType { get; set; }
+
+            public enum ClientTypes
+            {
+                [Icon("fas fa-file")]
+                [Header("Empty")]
+                [Description("Manual configuration")]
+                Empty = 0,
+                [Icon("fas fa-file-code")]
+                [Header("WebApplication")]
+                [Description("ServerSide (Auth Code Flow with PKCE)")]
+                Web = 1,
+                [Icon("fas fa-laptop")]
+                [Header("SPA")]
+                [Description("Javascript (Auth Code Flow with PKCE)")]
+                Spa = 2,
+                [Icon("fas fa-mobile")]
+                [Header("Native Application")]
+                [Description("Mobile/Desktop (Auth Code Flow with PKCE)")]
+                Native = 3,
+                [Icon("fas fa-server")]
+                [Header("Machine/Robot")]
+                [Description("Client Credentials flow")]
+                Machine = 4,
+                [Icon("fas fa-tv")]
+                [Header("Device flow")]
+                [Description("TV and Limited-Input Device Application")]
+                Device = 5
+            }
+        }
+
+        public IEnumerable<(ClientInputModel.ClientTypes, string header, string description, string icon)> GetClientTypes()
+        {
+            foreach (ClientInputModel.ClientTypes value in Enum.GetValues(typeof(ClientInputModel.ClientTypes)))
+            {
+                var header = value.GetAttributeOfType<HeaderAttribute>().Header;
+                var description = value.GetAttributeOfType<DescriptionAttribute>().Description;
+                var icon = value.GetAttributeOfType<IconAttribute>().Icon;
+                yield return (value, header, description, icon);
+            }
         }
 
         internal class ClientMappingConfiguration : Profile
@@ -59,18 +136,20 @@ namespace Xenial.Identity.Areas.Admin.Pages.Clients
                 {
                     var client = Mapper.Map(Input, new XpoClient(unitOfWork));
 
+                    PrepareClientTypeForNewClient(client);
+
                     await unitOfWork.SaveAsync(client);
                     await unitOfWork.CommitChangesAsync();
                     return Redirect("/Admin/Clients");
                 }
                 catch (ConstraintViolationException ex)
                 {
-                    logger.LogWarning(ex, "Error saving Client with {Name}", Input?.Name);
-                    ModelState.AddModelError($"{nameof(Input)}.{nameof(Input.Name)}", "Client name must be unique");
+                    logger.LogWarning(ex, "Error saving Client with {ClientName}", Input?.ClientName);
+                    ModelState.AddModelError($"{nameof(Input)}.{nameof(Input.ClientName)}", "Client name must be unique");
                 }
                 catch (Exception ex)
                 {
-                    logger.LogError(ex, "Error saving Client with {Name}", Input?.Name);
+                    logger.LogError(ex, "Error saving Client with {ClientName}", Input?.ClientName);
                     StatusMessage = $"Error saving client: {ex.Message}";
                     return Page();
                 }
@@ -79,6 +158,43 @@ namespace Xenial.Identity.Areas.Admin.Pages.Clients
             StatusMessage = "Error: Check Validation";
 
             return Page();
+        }
+
+        private void PrepareClientTypeForNewClient(XpoClient client)
+        {
+            switch (Input.ClientType)
+            {
+                case ClientInputModel.ClientTypes.Empty:
+                    break;
+                case ClientInputModel.ClientTypes.Web:
+                    client.AllowedGrantTypes.AddRange(CreateGrantTypes(GrantTypes.Code, client));
+                    client.RequirePkce = true;
+                    client.RequireClientSecret = true;
+                    break;
+                case ClientInputModel.ClientTypes.Spa:
+                    client.AllowedGrantTypes.AddRange(CreateGrantTypes(GrantTypes.Code, client));
+                    client.RequirePkce = true;
+                    client.RequireClientSecret = false;
+                    break;
+                case ClientInputModel.ClientTypes.Native:
+                    client.AllowedGrantTypes.AddRange(CreateGrantTypes(GrantTypes.Code, client));
+                    client.RequirePkce = true;
+                    client.RequireClientSecret = false;
+                    break;
+                case ClientInputModel.ClientTypes.Machine:
+                    client.AllowedGrantTypes.AddRange(CreateGrantTypes(GrantTypes.ClientCredentials, client));
+                    break;
+                case ClientInputModel.ClientTypes.Device:
+                    client.AllowedGrantTypes.AddRange(CreateGrantTypes(GrantTypes.DeviceFlow, client));
+                    client.RequireClientSecret = false;
+                    client.AllowOfflineAccess = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            static IEnumerable<XpoClientGrantType> CreateGrantTypes(IEnumerable<string> grantTypes, XpoClient client)
+                => grantTypes.Select(grant => new XpoClientGrantType(client.Session) { GrantType = grant });
         }
     }
 }
