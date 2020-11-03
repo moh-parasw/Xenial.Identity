c65b473d02286cf1a4f0d4f8ac11ef3b5ce2072c
Manuel Grundner <m.grundner@delegate.at>
2020-11-03 13:46:05 +0100

    build: npm

--- build/Program.cs
+++ build/Program.cs
@@ -14,13 +14,17 @@ var configuration = "Release";
 var selfContained = false;
 var packageAsSingleFile = false;
 
-Target("restore", () => RunAsync("dotnet", $"restore {sln}"));
-Target("build", DependsOn("restore"), () => RunAsync("dotnet", $"build {sln} --no-restore"));
+
+Target("restore:npm", () => RunAsync("cmd.exe", $"/C npm ci"));
+Target("restore:dotnet", () => RunAsync("dotnet", $"restore {sln}"));
+Target("restore", DependsOn("restore:npm", "restore:dotnet"));
+
+Target("build:npm", () => RunAsync("cmd.exe", $"/C npm run build"));
+Target("build:dotnet", DependsOn("restore:dotnet"), () => RunAsync("dotnet", $"build {sln} --no-restore"));
+Target("build", DependsOn("restore", "build:npm", "build:dotnet"));
 
 Target("publish", DependsOn("build"), () => RunAsync("dotnet", $"msbuild {web} /t:Restore;Build /p:Configuration={configuration} /p:RuntimeIdentifier=win-x64 /p:SelfContained={selfContained} /p:PackageAsSingleFile={packageAsSingleFile} /p:DeployOnBuild=true /p:WebPublishMethod=package /p:PublishProfile=Package /v:minimal /p:DesktopBuildPackageLocation={artifact} /p:DeployIisAppPath={iisPackageName}"));
-
 Target("deploy", DependsOn("publish"), () => RunAsync("cmd.exe", $"/C {projectName}.deploy.cmd /T /M:{Environment.GetEnvironmentVariable("WEBDEPLOY_IP")} /U:{Environment.GetEnvironmentVariable("WEBDEPLOY_USER")} /P:{Environment.GetEnvironmentVariable("WEBDEPLOY_PASS")} -allowUntrusted", workingDirectory: artifactsLocation));
-
 Target("default", DependsOn("publish"));
 
 await RunTargetsAndExitAsync(args);
