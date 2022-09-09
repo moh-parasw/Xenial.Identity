a2aa29accedb39c76e76fda9690a46eb7dc313ce
Manuel Grundner <m.grundner@delegate.at>
2022-09-09 13:22:16 +0200

    feat: MonacoEdit 2 way binding

--- client/js/MonacoEdit.ts
+++ client/js/MonacoEdit.ts
@@ -1,18 +1,32 @@
 import "../css/admin.scss";
 import loader from '@monaco-editor/loader';
+import type { editor } from "monaco-editor";
 
 type language = "html" | "json" | "css";
 
-export async function CreateMonacoEditor(el: HTMLElement, value: string, language: language) {
+const editors = new Map<HTMLElement, editor.IStandaloneCodeEditor>();
+
+export async function CreateMonacoEditor(component: DotNet.DotNetObject, el: HTMLElement, value: string, language: language) {
     loader.init().then(i => {
         const edit = i.editor.create(el, {
             value: value,
-            language: language
+            language: language,
+            theme: 'vs-dark',
+            minimap: {
+                enabled: false
+            },
         });
-        edit.onDidChangeModel(e => {
-            const newText = edit.getModel()?.getValue();
-            console.warn(newText);
-        })
+        editors.set(el, edit);
+        edit.onKeyUp(async _ => {
+            const newText = edit.getValue();
+            await component.invokeMethodAsync('UpdateValue', newText);
+        });
     });
+}
 
+export function DisposeMonacoEditor(el: HTMLElement){
+    const edit = editors.get(el);
+    if(edit){
+        edit.dispose();
+    }
 }
