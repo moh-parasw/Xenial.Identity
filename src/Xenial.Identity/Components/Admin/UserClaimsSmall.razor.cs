32ad3237f169daad01adea2b6186b40565f3651a
Manuel Grundner <m.grundner@delegate.at>
2022-09-11 13:56:58 +0200

    feat(UserClaims): Delete claim

---
+++ src/Xenial.Identity/Components/Admin/UserClaimsSmall.razor.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+using Xenial.Identity.Data;
+
+namespace Xenial.Identity.Components.Admin;
+
+public partial class UserClaimsSmall
+{
+    private async Task DeleteClaim(Claim claim)
+    {
+        currentClaim = claim;
+
+        var delete = await mbox.Show(new DialogOptions
+        {
+            MaxWidth = MaxWidth.Small,
+            FullWidth = true,
+        });
+
+        if (delete == true)
+        {
+            var result = await UserManager.RemoveClaimAsync(User, claim);
+            if (result.Succeeded)
+            {
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            Claim was successfully deleted!
+                        </li>
+                        <li>
+                           <em>{claim.Type}</em><br>
+                        </li>
+                    </ul>
+                    """, MudBlazor.Severity.Success);
+            }
+            else
+            {
+                var errors = string.Join("\n", result.Errors.Select(e => $"<li>Code: {e.Code}: {e.Description}</li>"));
+
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            There was an error when deleting the claim!
+                        </li>
+                        <li>
+                            <em>{claim.Type}</em><br>
+                        </li>
+                        {errors}
+                    </ul>
+                    """, MudBlazor.Severity.Error);
+            }
+        }
+        await Refresh();
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+        await Refresh();
+    }
+
+    private async Task Refresh() => claims = await UserManager.GetClaimsAsync(User);
+}
