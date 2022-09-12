038994ca20a4a1ea4c0f135e5732f053d3c29668
Manuel Grundner <m.grundner@delegate.at>
2022-09-12 10:19:33 +0200

    feat(Roles): Added roles page

---
+++ src/Xenial.Identity/Components/Admin/RoleClaimsSmall.razor.cs
@@ -0,0 +1,95 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+using Xenial.Identity.Data;
+
+namespace Xenial.Identity.Components.Admin;
+
+public partial class RoleClaimsSmall
+{
+    private async Task EditClaim(Claim claim = null)
+    {
+        var dialog = DialogService.Show<ClaimDialog>(
+            claim == null ? "Add Claim" : "Edit Claim",
+            new DialogParameters
+            {
+                [nameof(ClaimDialog.Claim)] = claim ?? new Claim("", ""),
+                [nameof(ClaimDialog.IsNewClaim)] = claim == null,
+                [nameof(ClaimDialog.User)] = Role,
+            },
+            new DialogOptions
+            {
+                MaxWidth = MaxWidth.Small,
+                FullWidth = true,
+                CloseOnEscapeKey = true,
+                DisableBackdropClick = true,
+            });
+
+        var refresh = await dialog.GetReturnValueAsync<bool?>();
+        if (refresh == true)
+        {
+            await Refresh();
+        }
+    }
+
+    private async Task DeleteClaim(Claim claim)
+    {
+        currentClaim = claim;
+
+        var delete = await mbox.Show(new DialogOptions
+        {
+            MaxWidth = MaxWidth.Small,
+            FullWidth = true,
+        });
+
+        if (delete == true)
+        {
+            var result = await RolesManager.RemoveClaimAsync(Role, claim);
+            if (result.Succeeded)
+            {
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            Claim was successfully deleted!
+                        </li>
+                        <li>
+                           <em>{claim.Type}</em><br>
+                        </li>
+                    </ul>
+                    """, MudBlazor.Severity.Success);
+            }
+            else
+            {
+                var errors = string.Join("\n", result.Errors.Select(e => $"<li>Code: {e.Code}: {e.Description}</li>"));
+
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            There was an error when deleting the claim!
+                        </li>
+                        <li>
+                            <em>{claim.Type}</em><br>
+                        </li>
+                        {errors}
+                    </ul>
+                    """, MudBlazor.Severity.Error);
+            }
+        }
+        await Refresh();
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+        await Refresh();
+    }
+
+    private async Task Refresh()
+    {
+        claims = await RolesManager.GetClaimsAsync(Role);
+        StateHasChanged();
+    }
+}
---
+++ src/Xenial.Identity/Components/Admin/Roles.razor.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+
+using Xenial.Identity.Data;
+
+namespace Xenial.Identity.Components.Admin;
+
+public partial class Roles
+{
+    private async Task DeleteRole(IdentityRole role)
+    {
+        var delete = await DialogService.ShowMessageBox("Delete Role", (MarkupString)$"""
+            <ul>
+                <li>
+                    Do your really want to delete the role?
+                </li>
+                <li>
+                    <em>{role.Name}</em>
+                </li>
+                <li>
+                    <strong>This operation can <em>not</em> be undone!</strong>
+                </li>
+            </ul>
+        """, yesText: "Delete!", cancelText: "Cancel");
+
+        if (delete == true)
+        {
+            var result = await RolesManager.DeleteAsync(role);
+            if (result.Succeeded)
+            {
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            Role was successfully deleted!
+                        </li>
+                        <li>
+                            <em>{role.Name}</em>
+                        </li>
+                    </ul>
+                    """, MudBlazor.Severity.Success);
+            }
+            else
+            {
+                var errors = string.Join("\n", result.Errors.Select(e => $"<li>Code: {e.Code}: {e.Description}</li>"));
+
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            There was an error when deleting the role!
+                        </li>
+                        <li>
+                            <em>{role.Name}</em>
+                        </li>
+                        {errors}
+                    </ul>
+                    """, MudBlazor.Severity.Error);
+            }
+        }
+
+        await table.ReloadServerData();
+    }
+}
