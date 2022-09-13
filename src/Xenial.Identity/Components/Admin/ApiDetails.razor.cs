85b4380758ecec571c9b340659da7751ac39a52e
Manuel Grundner <m.grundner@delegate.at>
2022-09-13 10:34:00 +0200

    feat(ApiDetails): Added Basic Edits

---
+++ src/Xenial.Identity/Components/Admin/ApiDetails.razor.cs
@@ -0,0 +1,40 @@
+﻿namespace Xenial.Identity.Components.Admin;
+
+public partial class ApiDetails
+{
+    protected async Task SaveRole()
+    {
+        try
+        {
+            await UnitOfWork.SaveAsync(Api);
+            await UnitOfWork.CommitChangesAsync();
+            await Reload();
+            Snackbar.Add($"""
+                <ul>
+                    <li>
+                        Api was successfully updated!
+                    </li>
+                    <li>
+                        <em>{Api.Name}</em>
+                    </li>
+                </ul>
+                """, MudBlazor.Severity.Success);
+        }
+        catch (Exception ex)
+        {
+            var errors = ex.Message;
+
+            Snackbar.Add($"""
+                <ul>
+                    <li>
+                        There was an error when updating the Api!
+                    </li>
+                    <li>
+                        <em>{Api.Name}</em>
+                    </li>
+                    {errors}
+                </ul>
+                """, MudBlazor.Severity.Error);
+        }
+    }
+}
---
+++ src/Xenial.Identity/Components/Admin/Clients.razor.cs
@@ -0,0 +1,66 @@
+﻿using DevExpress.Xpo;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+
+using Xenial.Identity.Data;
+using Xenial.Identity.Xpo.Storage.Models;
+
+namespace Xenial.Identity.Components.Admin;
+
+public partial class Clients
+{
+    private async Task Delete(XpoClient resource)
+    {
+        var delete = await DialogService.ShowMessageBox("Delete Client", (MarkupString)$"""
+            <ul>
+                <li>
+                    Do your really want to delete the Client?
+                </li>
+                <li>
+                    <em>{resource.ClientId}</em>
+                </li>
+                <li>
+                    <strong>This operation can <em>not</em> be undone!</strong>
+                </li>
+            </ul>
+        """, yesText: "Delete!", cancelText: "Cancel");
+
+        if (delete == true)
+        {
+            try
+            {
+                await UnitOfWork.DeleteAsync(resource);
+                await UnitOfWork.CommitChangesAsync();
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            Client was successfully deleted!
+                        </li>
+                        <li>
+                            <em>{resource.ClientId}</em>
+                        </li>
+                    </ul>
+                    """, MudBlazor.Severity.Success);
+            }
+            catch (Exception ex)
+            {
+                var errors = ex.Message;
+
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            There was an error when deleting the Client!
+                        </li>
+                        <li>
+                            <em>{resource.ClientId}</em>
+                        </li>
+                        {errors}
+                    </ul>
+                    """, MudBlazor.Severity.Error);
+            }
+        }
+
+        await table.ReloadServerData();
+    }
+}
