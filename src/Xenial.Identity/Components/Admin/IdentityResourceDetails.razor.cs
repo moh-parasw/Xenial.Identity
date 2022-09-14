250cf158372c48babe7fa9563636dd41cead94e6
Manuel Grundner <m.grundner@delegate.at>
2022-09-14 14:59:43 +0200

    feat(IdentityResource); Added Details

---
+++ src/Xenial.Identity/Components/Admin/IdentityResourceDetails.razor.cs
@@ -0,0 +1,41 @@
+﻿namespace Xenial.Identity.Components.Admin;
+
+public partial class IdentityResourceDetails
+{
+    protected async Task Save()
+    {
+        try
+        {
+            await UnitOfWork.SaveAsync(IdentityResource);
+            await UnitOfWork.CommitChangesAsync();
+
+            await Refresh();
+            Snackbar.Add($"""
+                <ul>
+                    <li>
+                        Role was successfully updated!
+                    </li>
+                    <li>
+                        <em>{IdentityResource.Name}</em>
+                    </li>
+                </ul>
+                """, MudBlazor.Severity.Success);
+        }
+        catch (Exception ex)
+        {
+            var errors = ex.Message;
+            Snackbar.Add($"""
+                <ul>
+                    <li>
+                        There was an error when updating the IdendityResource!
+                    </li>
+                    <li>
+                        <em>{IdentityResource.Name}</em>
+                    </li>
+                    {errors}
+                </ul>
+                """, MudBlazor.Severity.Error);
+        }
+
+    }
+}
