a44ce8a203399dd342d56f6db054eb9d4faee2af
Manuel Grundner <m.grundner@delegate.at>
2022-09-15 12:19:33 +0200

    feat(IdentityResourceDetails): Added Properties

---
+++ src/Xenial.Identity/Components/Admin/IdentityResourcePropertiesDialog.razor.cs
@@ -0,0 +1,86 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+
+using MudBlazor;
+
+using Xenial.Identity.Data;
+using Xenial.Identity.Xpo.Storage.Models;
+
+namespace Xenial.Identity.Components.Admin;
+
+public partial class IdentityResourcePropertiesDialog
+{
+    [CascadingParameter]
+    private MudDialogInstance MudDialog { get; set; }
+
+    [Parameter, EditorRequired]
+    public XpoIdentityResourceProperty Property { get; set; }
+
+    [Parameter]
+    public EventCallback<XpoIdentityResourceProperty> PropertyChanged { get; set; }
+
+    [Parameter, EditorRequired]
+    public XpoIdentityResource Resource { get; set; }
+
+    [Parameter]
+    public bool IsNew { get; set; }
+
+    private void Save()
+    {
+        Property.Key = key;
+        Property.Value = value;
+        MudDialog.Close(true);
+    }
+
+    private ClaimEditMode Mode { get; set; }
+
+    private string key;
+    private string value;
+
+    private void SetKey(string key)
+        => this.key = key;
+
+    private void SetValue(string value)
+        => this.value = value;
+
+    private void SetMode(ClaimEditMode mode)
+    {
+        Mode = mode;
+        if (mode == ClaimEditMode.Json && string.IsNullOrWhiteSpace(value))
+        {
+            value = """
+            {
+                "": ""
+            }
+            """;
+        }
+    }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        value = Property.Value;
+        key = Property.Key;
+        if (value?.StartsWith("{") ?? false)
+        {
+            Mode = ClaimEditMode.Json;
+        }
+        //if (value?.StartsWith("<") ?? false)
+        //{
+        //    Mode = ClaimEditMode.Xml;
+        //}
+    }
+
+    private void Cancel()
+        => MudDialog?.Close();
+
+    private enum ClaimEditMode
+    {
+        Text,
+        MultiLine,
+        Json,
+        //Xml
+    }
+}
---
+++ src/Xenial.Identity/Components/Admin/IdentityResourcePropertiesSmall.razor.cs
@@ -0,0 +1,113 @@
+﻿using DevExpress.Xpo;
+
+using MudBlazor;
+using Xenial.Identity.Xpo.Storage.Models;
+
+namespace Xenial.Identity.Components.Admin;
+
+public partial class IdentityResourcePropertiesSmall
+{
+    private async Task Edit(XpoIdentityResourceProperty property = null)
+    {
+        using (var uow = UnitOfWork.BeginNestedUnitOfWork())
+        {
+            var resource = uow.GetObjectByKey<XpoIdentityResource>(Resource.Id);
+            var currentProperty = property switch
+            {
+                { } => uow.GetObjectByKey<XpoIdentityResourceProperty>(property.Id),
+                _ => new XpoIdentityResourceProperty(uow) { IdentityResource = resource }
+            };
+            var dialog = DialogService.Show<IdentityResourcePropertiesDialog>(
+                property == null ? "Add Property" : "Edit Property",
+                new DialogParameters
+                {
+                    [nameof(IdentityResourcePropertiesDialog.Property)] = currentProperty,
+                    [nameof(IdentityResourcePropertiesDialog.IsNew)] = property == null,
+                    [nameof(IdentityResourcePropertiesDialog.Resource)] = resource,
+                },
+                new DialogOptions
+                {
+                    MaxWidth = MaxWidth.Small,
+                    FullWidth = true,
+                    CloseOnEscapeKey = true,
+                    DisableBackdropClick = true,
+                });
+
+            var refresh = await dialog.GetReturnValueAsync<bool?>();
+            if (refresh == true)
+            {
+                await uow.SaveAsync(currentProperty);
+                await uow.CommitChangesAsync();
+                await UnitOfWork.SaveAsync(Resource);
+                await UnitOfWork.CommitChangesAsync();
+                await Refresh();
+            }
+            else
+            {
+                uow.DropChanges();
+            }
+        }
+    }
+
+    private async Task Delete(XpoIdentityResourceProperty property)
+    {
+        currentProperty = property;
+
+        var delete = await mbox.Show(new DialogOptions
+        {
+            MaxWidth = MaxWidth.Small,
+            FullWidth = true,
+        });
+
+        if (delete == true)
+        {
+            try
+            {
+                await UnitOfWork.DeleteAsync(property);
+                await UnitOfWork.CommitChangesAsync();
+
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            Property was successfully deleted!
+                        </li>
+                        <li>
+                           <em>{property.Key}</em><br>
+                        </li>
+                    </ul>
+                    """, MudBlazor.Severity.Success);
+            }
+            catch (Exception ex)
+            {
+                var errors = ex.Message;
+
+                Snackbar.Add($"""
+                    <ul>
+                        <li>
+                            There was an error when deleting the property!
+                        </li>
+                        <li>
+                            <em>{property.Key}</em><br>
+                        </li>
+                        {errors}
+                    </ul>
+                    """, MudBlazor.Severity.Error);
+            }
+        }
+        await Refresh();
+    }
+
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+        await Refresh();
+    }
+
+    private async Task Refresh()
+    {
+        Resource.Properties.Reload();
+        await UnitOfWork.ReloadAsync(Resource, true);
+        properties = Resource.Properties.ToArray();
+        StateHasChanged();
+    }
+}
