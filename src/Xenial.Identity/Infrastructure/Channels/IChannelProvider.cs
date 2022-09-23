6b231c8d58c23b798e57ccdbd8bccc7ed07bb409
Manuel Grundner <m.grundner@delegate.at>
2022-09-23 12:45:46 +0200

    feat(Channels): Added channel interface stubs

---
+++ src/Xenial.Identity/Infrastructure/Channels/IChannelProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using Xenial.Identity.Models;
+
+namespace Xenial.Identity.Infrastructure.Channels;
+
+public interface ICommunicationChannel
+{
+    Task SetChannelSettings(string channelSettingsJson);
+}
+
+internal interface ICommunicationChannelRegistry
+{
+    internal IEnumerable<ICommunicationChannelRegistration> Registrations { get; }
+
+    ICommunicationChannel GetChannel(CommunicationChannelType channelType, string channelProviderType);
+
+    void AddChannel(ICommunicationChannelRegistration registration);
+}
+
+internal interface ICommunicationChannelRegistration
+{
+    string ProviderType { get; set; }
+    CommunicationChannelType Type { get; set; }
+    Type ChannelType { get; }
+    Type SettingsComponent { get; }
+    string DisplayName { get; set; }
+}
+
+public interface ICommunicationChannelOptions
+{
+    ICommunicationChannelOptions RegisterChannel<TChannel, TSettingsComponent>(
+        CommunicationChannelType channelType,
+        string channelProviderType,
+        string displayName
+    ) where TChannel : class, ICommunicationChannel
+      where TSettingsComponent : ComponentBase;
+}
